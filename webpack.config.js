a5d551da6e70943e4ddc81176aefa18beeac5018
Pablo <pfranco@outlook.com>
2022-07-27 10:39:04 +0200

    replaced code

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve('src', 'modules', 'module1.js'),
-    // entry: {
-
-    //     // module1: {
-    //     //     import: path.resolve('src', 'modules', 'module1.js'),
-    //     //     dependOn: 'vendor'
-    //     // },
-    //     // module2: {
-    //     //     import: path.resolve('src', 'modules', 'module2.js'),
-    //     //     dependOn: 'vendor'
-    //     // },
-    //     // vendor: ['lodash'],
-    // },
+    entry: {
+        module1: path.resolve('src', 'modules', 'module1.js'),
+        module2: path.resolve('src', 'modules', 'module2.js'),
+    },
 
     output: {
-        // filename: '[name].[contenthash].js',
-        filename: '[name].js',
-    },
+        filename: '[name].[contenthash].js',
+    }
 };
