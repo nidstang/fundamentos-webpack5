f0825b59c7ba990ed6e8a9a91865f23e6014c05c
Pablo <pfranco@outlook.com>
2022-07-27 13:58:49 +0200

    code

--- webpack.config.js
+++ webpack.config.js
@@ -3,83 +3,86 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const CopyWebpackPlugin = require('copy-webpack-plugin');
 // const HashInfoPlugin = require('./plugins/HashInfoPlugin');
 
-module.exports = {
-    entry: path.resolve('src', 'app.js'),
+module.exports = (_, argv) => {
+    return {
+        // mode: 'development', // none | development | production
+        entry: path.resolve('src', 'app.js'),
 
-    output: {
-        filename: 'main.[contenthash].js',
-    }, 
+        output: {
+            filename: 'main.[contenthash].js',
+        }, 
 
-    devServer: {
-        port: 3000,
-    },
+        devServer: {
+            port: 3000,
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.(jsx|js)$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
+        module: {
+            rules: [
+                {
+                    test: /\.(jsx|js)$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-react', '@babel/preset-env'],
+                        }
                     }
-                }
-            },
+                },
 
-            {
-                test: /\.tsx$/,
-                use: 'ts-loader',
-            },
+                {
+                    test: /\.tsx$/,
+                    use: 'ts-loader',
+                },
 
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
+                {
+                    test: /\.(css|scss)$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
 
-            {
-                test: /\.(ttf|png)$/,
-                type: 'asset' // asset/resource | asset/inline, asset. asset/source
-            },
+                {
+                    test: /\.(ttf|png)$/,
+                    type: 'asset' // asset/resource | asset/inline, asset. asset/source
+                },
 
-            {
-                test: /\.txt$/,
-                type: 'asset/source',
-            },
+                {
+                    test: /\.txt$/,
+                    type: 'asset/source',
+                },
+            ]
+        },
+
+        resolve: {
+            extensions: ['.js', '.jsx', '.tsx'],
+
+            alias: {
+                components: path.resolve('src', 'components'),
+                mood$: path.resolve('src', 'components', 'SelectMood.jsx'),
+            }
+
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'ToSet',
+                template: path.resolve('index.html'),
+            }),
+
+            // new CopyWebpackPlugin({
+            //     patterns: [
+            //         {
+            //             from: '*.txt', // default: output.path
+            //             to: 'files/[name].[contenthash].txt',
+            //             context: 'src/',
+            //         }
+            //     ]
+            // }),
+
+            // new HashInfoPlugin({
+            //     debug: true
+            // }),
         ]
-    },
-
-    resolve: {
-        extensions: ['.js', '.jsx', '.tsx'],
-
-        alias: {
-            components: path.resolve('src', 'components'),
-            mood$: path.resolve('src', 'components', 'SelectMood.jsx'),
-        }
-
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'ToSet',
-            template: path.resolve('index.html'),
-        }),
-
-        // new CopyWebpackPlugin({
-        //     patterns: [
-        //         {
-        //             from: '*.txt', // default: output.path
-        //             to: 'files/[name].[contenthash].txt',
-        //             context: 'src/',
-        //         }
-        //     ]
-        // }),
-
-        // new HashInfoPlugin({
-        //     debug: true
-        // }),
-    ]
+    }
 };
