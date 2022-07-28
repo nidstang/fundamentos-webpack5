166c15655c266ed23ce1d3e5d4c90441b159494c
Pablo <pfranco@outlook.com>
2022-07-28 12:38:52 +0200

    wip

--- src/TosetApp.jsx
+++ src/TosetApp.jsx
@@ -1,11 +1,9 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState } from 'react';
 import Logo from './components/Logo.jsx';
 import Register from './components/Register.jsx';
-// import YourSets from './components/YourSets.jsx';
-// import './assets/styles.scss';
+import YourSets from './components/YourSets.jsx';
+import './assets/styles.scss';
 import './assets/main.css';
-
-const YourSets = React.lazy(() => import('./components/YourSets.jsx'));
 
 export default () => {
 
@@ -21,9 +19,7 @@ export default () => {
         <div className='container'>
             <Logo />
             <Register onNew={onNew} />
-            <Suspense fallback={<h2>Loading...</h2>}>
-                { sets.length > 0 && <YourSets sets={sets} /> }
-            </Suspense>
+            <YourSets sets={sets} />
         </div>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HashInfoPlugin = require('./plugins/HashInfoPlugin');
 
 module.exports = (env, argv) => ({
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         filename: '[name].[contenthash].js',
-    },
-
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-
-            minSize: 0, // ultimo
-            maxInitialRequests: Infinity,
-
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                        name: module => {
-                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                            return `npm.${packageName}`;
-                        },
-                }
-                // vendors: {
-                //     minSize: 1000, // el tamaño minimo para que se cree un chunk
-                //     name: 'vendors',
-                //     priority: 1,
-                //     test: /[\\/]node_modules[\\/]/,
-                // },
-
-                // react: {
-                //     name: 'react',
-                //     priority: 2,
-                //     test: /[\\/]node_modules[\\/](react|react-dom)/,
-                // },
-
-                // asyncModules: {
-                //     // minSize: 0,
-                //     name: 'dynamic',
-                //     chunks: 'async',
-                // }
-
-                // react: {
-                //     name: 'react',
-                //     priority: 2,
-                //     test(module) {
-                //         return module.resource.includes('react');
-                //     }
-                // }
-            }
-        },
-
-        runtimeChunk: {
-            name: 'runtime',
-        }
     },
 
     module: {
@@ -79,7 +27,7 @@ module.exports = (env, argv) => ({
             {
                 test: /\.(css|scss)$/,
                 use: [
-                    argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'style-loader',
                     'css-loader',
                     'sass-loader',
                 ]
@@ -90,20 +38,10 @@ module.exports = (env, argv) => ({
                 type: 'asset/source',
             },
 
-            // {
-            //     test: /\.(png|ttf)$/,
-            //     type: 'asset/inline', // no genera ninguno
-            // }
-
             {
                 test: /\.png$/,
-                type: 'asset' // solo mete el logo
+                type: 'asset', 
             },
-
-            // {
-            //     test: /\.png$/,
-            //     type: 'asset/inline'
-            // },
         ]
     },
 
@@ -124,35 +62,11 @@ module.exports = (env, argv) => ({
         port: 3000,
     },
 
-    // devtool: 'eval-cheap-source-map', // development: build ok, rebuild fast -> transformed code, buena info stack
-    // devtool: 'eval-cheap-module-source-map', // development: build slow, rebuild fast -> original code
-    // devtool: 'hidden-source-map', // production: build slowest, rebuild slowest -> se carga pero no se referencia
-    // devtool: 'source-map', // production: build slowest, rebuild slowest -> genera fichero aparte
-
     plugins: [
         new HtmlWebpackPlugin({
             title: 'My App',
             template: path.resolve('index.html'),
-            // scriptLoading: 'blocking',
             meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
         }),
-
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'assets/*',
-                    to: 'files/[name].[contenthash].[ext]',
-                    context: 'src/'
-                }
-            ]
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-
-        // new HashInfoPlugin({
-        //     fileName: 'data.json',
-        // }),
     ],
 });
